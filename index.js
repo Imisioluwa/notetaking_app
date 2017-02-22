e4e7f7f91cbf1d8ffc86813286f363f613377872
Imisioluwa <imizezek@yahoo.com>
2017-02-22 12:50:26 -0800

    Add commands deletenode listnotes and next

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const vorpal = require('vorpal')();
 const notesApp = require('./app.js');
 const CLI = require('./cli.js');
-
+//Instantiating notesApp
 let NoteApp = new notesApp();
+//Instantiating a new object 'CLI' which passes vorpal
 let cli = new CLI(vorpal);
 
 vorpal
-.command('signup', 'create a WriteSmart account')
+//Initiates command for signup
+.command('signup', 'Create a WriteSmart account')
+//Passes args which contain parameter and callback
 .action((args, cb) => {
+	//Get credentials for Signup and login activity
 	cli.getCredentials()
+	//resolves credentials
 	.then((credentials) => {
 		NoteApp.signup(credentials)
 		.then((message) => {
@@ -19,7 +24,8 @@ vorpal
 });
 
 vorpal
-.command('login', 'login to your WriteSmart account')
+//Initiates command for login
+.command('login', 'Login to your WriteSmart account')
 .action((args, cb) => {
 	cli.getCredentials()
 	.then((credentials) => {
@@ -30,24 +36,51 @@ vorpal
 		});
 	});
 });
-
+  //Initiates command for createnote
 vorpal
-.command('createnote', 'write a note')
+.command('createnote', 'Write a note')
 .action((args, cb) => {
 	cli.writeNote()
 	.then((note) => {
-		cli.writeNoteOutput(NoteApp.createNote(note));
+		cli.createNoteOutput(NoteApp.createNote(note));
 		cb();
 	});
 });
-
+   //Initiates command for viewnote
 vorpal
-.command('viewnote <note_id>', 'view a single note')
+.command('viewnote <note_id>', 'View a single note')
 .action((args, cb) => {
-	cli.getNoteOutput(NoteApp.getNote(args.note_id));
+	cli.viewNoteOutput(NoteApp.getNote(args.note_id));
 	cb();
 });
-
+  //Initiates command for deletenote
+vorpal
+.command('deletenote <note_id>', "Delete a single note")
+.action((args, cb) => {
+	cli.deleteNoteOutput(NoteApp.deleteNote(args.note_id));
+	cb();
+});
+  //Initiates command for listnotes
+vorpal
+.command('listnotes', "List all notes")
+.option('-l, --limit <num>', "Number of items to display on a page at once")
+.validate((args) => {
+	if(args.options.limit) {
+		return Number.isInteger(args.options.limit) || "limit must be an integer";
+	}
+})
+.action((args, cb) => {
+	cli.listNotesOutput(NoteApp.getAllNotes(args.options.limit));
+	cb();
+});
+   //Calls the next sets of notes
+vorpal
+.command('next', "See the next set of data in the running query")
+.action((args, cb) => {
+	cli.listNotesOutput(NoteApp.next());
+	cb();
+});
+//Customize console prompt
 vorpal
 .delimiter('WriteSmart >>> ')
 .show();
